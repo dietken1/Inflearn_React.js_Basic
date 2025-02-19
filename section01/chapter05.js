5a4558871eae6e1daf1a9ffff026556c43853e5a
dietken1 <dietken1@ajou.ac.kr>
2025-02-20 00:14:10 +0900

    1.5) 자료형

---
+++ section01/chapter05.js
@@ -0,0 +1,38 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+let nan = NaN;
+console.log(inf);
+console.log(mInf);
+console.log(nan);
+
+// 2. String Type
+let myName = '정원준'
+let myLocation = '동탄'
+let introduce = myName + ' ' + myLocation
+let introduceText = `${myName}은 ${myLocation}에 거주합니다.`
+console.log(introduce);
+console.log(introduceText);
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+console.log(isSwitchOn);
+console.log(isEmpty);
+
+// 4. Null Type (아무것도 없음)
+let empty = null;
+console.log(empty);
+
+// 5. Undefined Type (아직 처리하지 못한 값 or 아직 정의되지 않은 값)
+let none;
+console.log(none);
