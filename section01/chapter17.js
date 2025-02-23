e355a1f98282a2a53b69e7649fb4a743ddcc18e8
dietken1 <dietken1@ajou.ac.kr>
2025-02-23 17:50:50 +0900

    1-17) 배열

---
+++ section01/chapter17.js
@@ -0,0 +1,25 @@
+// 1. 배열 생성
+let arrA = new Array();  // 배열 생성자
+let arrB = [];  // 배열 리터럴 (주로 사용)
+
+let arrC = [
+    1,
+    2,
+    3,
+    true,
+    'hello',
+    null,
+    undefined,
+    () => {},
+    {},
+    [],
+];
+
+// 2. 배열 요소 접근
+let item1 = arrC[0];
+let item2 = arrC[5];
+console.log(item1,item2);
+
+console.log(arrC[3]);
+arrC[3] = false;
+console.log(arrC[3]);
