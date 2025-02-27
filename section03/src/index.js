5f807c81195c8631ce97a56c40bde4fa2edadc00
dietken1 <dietken1@ajou.ac.kr>
2025-02-27 23:18:42 +0900

    3-5) Node.js  라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -4,3 +4,9 @@ import mul, { add,sub } from "./math.js";
 console.log(add(3,4));
 console.log(sub(3,4));
 console.log(mul(3,4));
+
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
