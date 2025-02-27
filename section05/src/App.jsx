5cc734fe8194a9f8d86bbdbc6bc46b2e3cede548
dietken1 <dietken1@ajou.ac.kr>
2025-02-28 01:45:35 +0900

    5-2) React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
+
+// 컴포넌트 이름은 대문자로 시작해야함
 function App() {
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
