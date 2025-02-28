2dffca250d8abbe7ff082c383b58e672034324ce
dietken1 <dietken1@ajou.ac.kr>
2025-02-28 10:21:37 +0900

    5-4) Props로 데이터 전달하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,15 +2,28 @@ import './App.css'
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
-
+import Button from './components/Button';
 
 // 컴포넌트 이름은 대문자로 시작해야함
 function App() {
+
+  // Spread연산자를 활용한 props전달
+  const buttonProps = {
+    text : '메일',
+    color : 'red',
+    a : 1,
+    b : 2,
+    c : 3,
+  };
+
   return (
     <>
+     <Button {...buttonProps} />
+     <Button text={'카페'} />
+     <Button text={'블로그'} >
+      {/* children 키워드를 활용한 props전달 */}
       <Header />
-      <Main />
-      <Footer />
+      </Button>
     </>
   );
 }
