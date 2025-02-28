80a41f8307fbdf63baefa62ed74b6058b7dc2f81
dietken1 <dietken1@ajou.ac.kr>
2025-02-28 10:52:03 +0900

    5-6) State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,31 +1,32 @@
 import './App.css'
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import Button from './components/Button';
+import { useState } from 'react';
 
 // 컴포넌트 이름은 대문자로 시작해야함
 function App() {
-
-  // Spread연산자를 활용한 props전달
-  const buttonProps = {
-    text : '메일',
-    color : 'red',
-    a : 1,
-    b : 2,
-    c : 3,
-  };
-
+  // 리액트에서는 State의 값이 변경되어야 리렌더링이 된다
+  // -> 즉, 일반 변수의 값이 바뀐다고 그것이 바로 반영되는것은 아님!
+  // -> 그래서 useState를 사용함!
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState('OFF');
   return (
     <>
-     <Button {...buttonProps} />
-     <Button text={'카페'} />
-     <Button text={'블로그'} >
-      {/* children 키워드를 활용한 props전달 */}
-      <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          light === 'OFF' ? setLight('ON') : setLight('OFF');
+        }}>
+          {light === 'OFF' ? '켜기' : '끄기'}
+        </button>
+      </div>
+
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => {setCount(count + 1);}}>
+          +
+        </button>
+      </div>
     </>
   );
 }
 
-export default App
+export default App;
