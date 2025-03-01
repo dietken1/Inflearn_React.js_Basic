d8f8a0e45eb3c2f8aafc76ae95ffe937ba33d646
dietken1 <dietken1@ajou.ac.kr>
2025-03-02 02:19:13 +0900

    10-2) useMemo와 연산 최적화

--- section10/src/components/List.jsx
+++ section10/src/components/List.jsx
@@ -1,6 +1,6 @@
 import './List.css'
 import TodoItem from './TodoItem'
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
 const List = ({todos, onUpdate, onDelete}) => {
     const [search, setSearch] = useState('');
@@ -22,9 +22,40 @@ const List = ({todos, onUpdate, onDelete}) => {
 
     const filteredTodos = getFilteredData();
 
+    const getAnalyzedData = () => {
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo)=>todo.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        };
+    };
+
+    useMemo(()=>{
+        const totalCount = todos.length;
+        const doneCount = todos.filter((todo)=>todo.isDone).length;
+        const notDoneCount = totalCount - doneCount;
+
+        return {
+            totalCount,
+            doneCount,
+            notDoneCount
+        };
+    }, [todos]);
+
+    const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
     return (
         <div className="List">
             <h4>Todo List👀</h4>
+            <div>
+                <div>total : {totalCount}</div>
+                <div>done : {doneCount}</div>
+                <div>notDone : {notDoneCount}</div>
+            </div>
             <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요" />
             <div className='todos_wrapper'>
             {filteredTodos.map((todo) => { // 리액트에서 리스트로 나타내는 방법
