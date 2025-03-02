20243593dc0cdf31fd13d1df04b88ea007bf8c05
dietken1 <dietken1@ajou.ac.kr>
2025-03-02 20:24:21 +0900

    12-4) 페이지 라우팅 3.페이지 이동

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Link, useNavigate} from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
 import Diary from './pages/Diary'
@@ -9,16 +9,30 @@ import NotFound from './pages/NotFound'
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
+  const nav = useNavigate();
+
+  const onClickButton = (() => {
+    nav('/new');
+  });
+
   return (
-    // 1. Routes컴포넌트 안에는 Route컴포넌트만 사용 가능
-    // 2. Routes컴포넌트 외부의 요소는 모든 페이지에 공통적으로 렌더링됨
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/new' element={<New />} />
-      <Route path='/diary' element={<Diary />} />
-      <Route path='/*' element={<NotFound />} />
-    </Routes>
-  )
+    <>
+      <div>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/new'}>New</Link>
+        <Link to={'/diary'}>Diary</Link>
+      </div>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      {/* 1. Routes컴포넌트 안에는 Route컴포넌트만 사용 가능 */}
+      {/* 2. Routes컴포넌트 외부의 요소는 모든 페이지에 공통적으로 렌더링됨 */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/diary' element={<Diary />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
