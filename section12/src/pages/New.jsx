4dff05ad4c140dabbf1adaee64cef9555017e8ad
dietken1 <dietken1@ajou.ac.kr>
2025-03-03 19:42:56 +0900

    12-13) New 페이지 구현하기 1. UI

--- section12/src/pages/New.jsx
+++ section12/src/pages/New.jsx
@@ -1,8 +1,15 @@
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Editor from '../components/Editor'
+
 const New = () => {
     return (
-        <>
-        New
-        </>
+        <div>
+            <Header title={'새 일기 쓰기'}
+                leftChild={<Button text={'< 뒤로 가기'} />}
+            />
+            <Editor />
+        </div>
     );
 };
 
